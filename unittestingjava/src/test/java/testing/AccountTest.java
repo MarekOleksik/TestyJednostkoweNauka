6136ceb2d976213e9b18c56d624f03516448c69b
Marek <marek@pw.edu.pl>
2019-08-19 00:25:31 +0200

    postęp do lekcji 12

---
+++ unittestingjava/src/test/java/testing/AccountTest.java
@@ -0,0 +1,30 @@
+package testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AccountTest {
+    @Test
+    void newAccountShouldNotBeActiveAfterCreation() {
+        //given
+        Account newAccount = new Account();
+
+        //then
+       assertFalse(newAccount.isActive());
+
+    }
+    @Test
+    void accountShouldBeActiveAfterActivation() {
+        //given
+        Account newAccount = new Account();
+        assertFalse(newAccount.isActive());
+
+        //when
+        newAccount.activate();
+
+        //then
+        assertTrue(newAccount.isActive());
+    }
+}
